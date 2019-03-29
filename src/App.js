77a366dc89422ea58b30082028b33b3677a5e0e9
khenryjoe <joseph.ketterer@gmail.com>
2019-03-29 19:53:25 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -2,27 +2,75 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+function Child({ onAction }) {
+  return (
+    <button onClick={onAction}>
+      Click Me!
+    </button>
+  );
+}
+
+function Reset({resetButton}){
+  return(
+    <button onClick={resetButton}>
+      Reset
+    </button>
+  )
+}
+
+
+class CountingParent extends
+React.Component {
+  state = {
+    actionCount: 0
+  };
+
+  reset = press => {
+    console.log(press);
+
+    this.setState({
+      actionCount: 0
+    });
+
+  };
+
+  handleAction = action => {
+    console.log(
+      "Child says",
+      action
+    );
+
+    // actionCount is incremented,
+    // and the new count replaces
+    // the existing one
+    this.setState({
+      actionCount:
+        this.state.actionCount + 1
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Child
+          onAction={
+            this.handleAction
+          }
+        />
+        <Reset resetButton ={
+          this.reset}/>
+        <p>
+          Clicked
+          {" "}
+          {this.state.actionCount}
+          {" "}
+          times
+        </p>
       </div>
     );
   }
 }
+ 
 
-export default App;
+export default CountingParent;
